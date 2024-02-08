c31b69361d0988578e04519be32631618f8bd746
Betulioo <luis.nunez@bootcamp-upgrade.com>
2024-02-08 21:56:00 +0100

    almost

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -8,15 +8,38 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class TareaController extends AbstractController
 {
+    
     #[Route("/tareas")]
     public function getTareas()
     {
         $tasks = file_get_contents($this->getParameter("kernel.project_dir")."/data/tasks.json");
 
-        return new Response($tasks, headers: ['Content-Type' => 'application/json']);
+        return new Response($tasks, headers: ['Content-Type' => 'application/json', 'Access-Control-Allow-Origin' => '*']);
+    }
+
+    #[Route("/tarea/{id}", methods: ['GET'])]
+    public function getById($id)
+    {
+
+        $tasks = file_get_contents($this->getParameter("kernel.project_dir") . "/data/tasks.json");
+        $arrayTasks = json_decode($tasks, true);
+
+        $foundTask = null;
+        foreach ($arrayTasks['tareas'] as $task) {
+            if ($task['identificador'] == $id) {
+                $foundTask = $task;
+                break;
+            }
+        }
+
+        if ($foundTask === null) {
+            return new Response("Tarea no encontrada", Response::HTTP_NOT_FOUND);
+        }
+
+        return new Response(json_encode($foundTask), headers: ['Content-Type' => 'application/json']);
+
     }
 
     #[Route("/tarea", methods: ['POST'])]
@@ -32,7 +55,10 @@ class TareaController extends AbstractController
         $arrayTasks['tareas'][] = $arrayTask;
 
         file_put_contents($this->getParameter("kernel.project_dir")."/data/tasks.json", json_encode($arrayTasks));
-        return new Response("OK");
+
+        
+        return new Response("OK", headers: ['Access-Control-Allow-Methods'=> 'POST','Access-Control-Allow-Origin' => '*'
+     ]);
     }
 
     #[Route("/tarea/{id}", methods: ['DELETE'])]
@@ -62,7 +88,6 @@ class TareaController extends AbstractController
         $tasks = file_get_contents($this->getParameter("kernel.project_dir")."/data/tasks.json");
         $arrayTasks = json_decode($tasks, true);
 
-        $removeId = false;
         foreach ($arrayTasks['tareas'] as $key => $task){
             if ($task['identificador'] == $id){
                 $arrayTasks['tareas'][$key]['estado'] = 'hecha';
@@ -71,5 +96,39 @@ class TareaController extends AbstractController
         }
 
         return new Response("OK");
+    }
+
+    #[Route("/tarea/{id}/undone")]
+    public function tareaSinHacer($id)
+    {
+        $tasks = file_get_contents($this->getParameter("kernel.project_dir")."/data/tasks.json");
+        $arrayTasks = json_decode($tasks, true);
+
+        foreach ($arrayTasks['tareas'] as $key => $task){
+            if ($task['identificador'] == $id){
+                $arrayTasks['tareas'][$key]['estado'] = 'Sin Hacer';
+                file_put_contents($this->getParameter("kernel.project_dir")."/data/tasks.json", json_encode($arrayTasks));
+            }
+        }
+
+        return new Response("OK");
+    }
+
+    #[Route("/tarea/{id}", methods:['PUT'])]
+    public function editTarea(Request $request, $id)
+    {
+        $tasks = file_get_contents($this->getParameter("kernel.project_dir")."/data/tasks.json");
+        $arrayTasks = json_decode($tasks, true);
+
+        $updatedTask = json_decode($request->getContent(), true);
+
+        foreach ($arrayTasks['tareas'] as $key => $task) {
+           if($task['identificador'] == $id){
+            $arrayTasks['tareas'][$key] = array_merge($task, $updatedTask);
+                file_put_contents($this->getParameter("kernel.project_dir") . "/data/tasks.json", json_encode($arrayTasks));
+                return new Response("Tarea actualizada", Response::HTTP_OK);
+           }
+        }
+        return new Response("Tarea no encontrada", Response::HTTP_NOT_FOUND);
     }
 }
